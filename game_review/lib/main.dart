c8303b1228f440b435487993744683eed0e17604
Nikola <nikolandric123@gmail.com>
2025-10-02 16:46:07 +0200

    Added missing import in main.dart.

--- game_review/lib/main.dart
+++ game_review/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:game_review/common/dependency_injection/injection_container.dart';
 import 'i18n/strings.g.dart';
 
 import 'package:game_review/features/welcome_screen/welcome_page.dart';
