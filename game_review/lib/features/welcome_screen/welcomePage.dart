36ccbadbd8620fb80f8ec83fb9a7d46e084681e0
Nikola <nikolandric123@gmail.com>
2025-10-02 08:52:21 +0200

    Removed unnecessary app bar, put TODO comments, switched text style to button style instead of child, formatted document to standard dart format, removed Containers and moved border styles to button it self.

--- game_review/lib/features/welcome_screen/welcomePage.dart
+++ game_review/lib/features/welcome_screen/welcomePage.dart
@@ -8,74 +8,87 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          Image(image: AssetImage('lib/common/assets/images/Games.png'),
-          fit: BoxFit.cover,),
-          Image(image: AssetImage('lib/common/assets/images/Logo.png')),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: FilledButton(onPressed: () {
-
-                    },
-                      style: FilledButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-
-                        ),
-                      ),
-
-                      child: Text("Login",
-                        style: TextStyle(fontSize: 20,
-                            color: Colors.deepPurple),),),
-
-                  )
-              )
-            ],
+          Positioned(
+            top: -100,
+            left: 0,
+            right: 0,
+            child: Image(
+              image: AssetImage('lib/common/assets/images/Games.png'),
+              fit: BoxFit.fill,
+            ),
           ),
-
-          SizedBox(height: 8.0,),
-
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
+
             children: [
-              Expanded(
-                  child:  Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child:
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(20.0)
-                  ),
-                  child: FilledButton(onPressed: () {
-
-                },
-                  style: FilledButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-
+              SizedBox(height: 250),
+              Image(image: AssetImage('lib/common/assets/images/Logo.png')),
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: FilledButton(
+                        onPressed: () {
+                          //TODO: Implement on pressed navigation to login page
+                        },
+                        style: FilledButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Color.fromRGBO(42, 1, 68, 1),
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          textStyle: TextStyle(
+                            fontSize:
+                                30, //TODO: These fixed values for size and colors should be changed for default theme values
+                            fontWeight: FontWeight.bold,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        child: Text("Login"),
+                      ),
                     ),
                   ),
+                ],
+              ),
 
-                  child: Text("Register",
-                      style: TextStyle(fontSize: 20,
-                          color: Colors.white),),),
+              SizedBox(height: 8.0),
 
-                )
-                  )
-               )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: FilledButton(
+                        onPressed: () {
+                          //TODO: Implement on pressed navigation to login page
+                        },
+                        style: FilledButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          foregroundColor: Colors.white,
+                          side: BorderSide(color: Colors.white),
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          textStyle: TextStyle(
+                            fontSize:
+                                30, //TODO: These fixed values for size and colors should be changed for default theme values
+
+                            fontWeight: FontWeight.bold,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        child: Text("Register"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ],
