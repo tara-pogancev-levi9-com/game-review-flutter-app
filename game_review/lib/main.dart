abd95c01b835c8321ab03479a68256e16c3d76d7
vecca49 <148877380+vecca49@users.noreply.github.com>
2025-10-02 13:57:41 +0200

    Added localization for English and Serbian. (#9)

--- game_review/lib/main.dart
+++ game_review/lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'i18n/strings.g.dart';
+
 import 'package:game_review/common/dependency_injection/injection_container.dart';
 import 'package:game_review/features/welcome_screen/welcomePage.dart';
 void main() {
-  setup();
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  LocaleSettings.setLocaleSync(AppLocale.en);
+  runApp(TranslationProvider(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: TranslationProvider.of(context).flutterLocale,
+      supportedLocales: AppLocaleUtils.supportedLocales,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
